14ab1156be69e6d645a9682b0f1e1fc8e535faf0
Rodion Rybko <rybkorodion201@mail.ru>
2026-02-24 21:41:21 +0700

    Добавил tokenStorage

--- clientPanel/src/app/api/instance.ts
+++ clientPanel/src/app/api/instance.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import {tokenStorage} from "../storage/tokenStorage";
 
 const instance = axios.create({ baseURL: 'http://localhost:4004' })
+
+
+instance.interceptors.request.use((config) => {
+    const token = tokenStorage.getItem()
+    if (token) {
+        config.headers = config.headers ?? {}
+        config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+})
 
 export { instance }
