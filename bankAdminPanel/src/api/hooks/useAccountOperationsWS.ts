a27b6bc40352f1c25444f4810762e20c9ca4ceb0
Maksim <67355664+Drak0ndr@users.noreply.github.com>
2026-03-19 21:16:27 +0700

    arm: add ws

--- bankAdminPanel/src/api/hooks/useAccountOperationsWS.ts
+++ bankAdminPanel/src/api/hooks/useAccountOperationsWS.ts
@@ -1,18 +1,33 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
+import type { AccountOperation } from '../../../generated/api/core'
+import { useQueryClient } from '@tanstack/react-query'
 export const useAccountOperationsWS = (id: string) => {
+   const [operations, setOperations] = useState<AccountOperation[]>([])
+   const [isLoading, setIsLoading] = useState(true)
+   const queryClient = useQueryClient()
+
    useEffect(() => {
       const socket = new WebSocket(
          `ws://localhost:4003/ws/accounts/${id}/operations?authorization=Bearer ${Cookies.get('accessToken')}`
       )
       socket.onopen = () => {
-         console.log('WebSocket connection opened')
+         setIsLoading(false)
       }
       socket.onmessage = (event) => {
-         console.log('Received message:', event.data)
+         const data = JSON.parse(event.data)
+         if (data.type === 'snapshot' && Array.isArray(data.items)) {
+            setOperations(data.items)
+         }
+         if (data.type === 'operation_added' && 'operation' in data) {
+            setOperations((prev) => [data.operation, ...prev])
+            queryClient.invalidateQueries({ queryKey: ['account'] })
+         }
       }
       return () => {
          socket.close()
       }
    }, [])
+
+   return { operations, isLoading }
 }
